e791fcccc40827eadea6acb78696bc42f4796e41
nicoviera <nicovieram10@gmail.com>
2021-10-23 17:24:53 -0300

    agregando tags

--- main.tf
+++ main.tf
@@ -6,10 +6,17 @@ provider "aws" {
 resource "aws_instance" "web" {
   count = 2
   ami = "ami-09e67e426f25ce0d7"
-
   instance_type = "t2.micro"
+  tags = {
+    Name = "devops-${count.index}"
+  }
+
 }
 
 output "public_ip" {
   value = "${aws_instance.web.*.public_ip}"
+}
+
+output "private_ip" {
+  value = "${aws_instance.web.*.private_ip}"
 }
